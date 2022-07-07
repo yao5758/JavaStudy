5018cc71f2d13e5aea67fdb83973adad75e8d232
chenyao <491005758@qq.com>
2022-07-07 18:06:03 +0800

    代码初始化

---
+++ src/main/java/com/cy/sourcecodestudy/SourceCodeStudyApplication.java
@@ -0,0 +1,13 @@
+package com.cy.sourcecodestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourceCodeStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SourceCodeStudyApplication.class, args);
+    }
+
+}
